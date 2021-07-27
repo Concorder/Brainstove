51849d8fe4edabb01f8035103d3a33be138c5fc8
Concorder <arenko.design@gmail.com>
2021-07-27 11:30:26 +0300

    Game_info edits

--- js/login.js
+++ js/login.js
@@ -52,11 +52,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let figure3 = document.getElementById('figure3');
 toStrand();
 setTimeout(toStrand, 200);
-setInterval(toStrand, 3000)
+setInterval(toStrand, 3000);
     function toStrand() {
             randomPosition(figure1);
             randomPosition(figure2);
             randomPosition(figure3);
-
     }
 });
